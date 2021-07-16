551efb81eb6e268fbb173f90db0df0d8c505d3c9
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-07-16 10:51:22 +0100

    [PRMT-1900] Encrypted terraform state s3 bucket

--- s3.tf
+++ s3.tf
@@ -21,6 +21,14 @@ resource "aws_s3_bucket" "prm-deductions-terraform-state" {
     }
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
      Name = "Terraform states of deductions infrastructure"
      CreatedBy = "prm-deductions-support-infra"
